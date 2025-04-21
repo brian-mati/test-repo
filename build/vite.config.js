59fec0e545d67c839d787796e1ac872708f90b05
brian-mati <brianmuthomimati.com>
2025-04-21 15:45:46 +0300

    tailwind added test

--- build/vite.config.js
+++ build/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import adonisjs from '@adonisjs/vite/client';
+import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         adonisjs({
             entrypoints: ['resources/css/app.css', 'resources/js/app.js'],
             reload: ['resources/views/**/*.edge'],
